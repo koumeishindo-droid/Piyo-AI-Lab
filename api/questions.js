aeac7c827853c48d0ee39d4bac691ad418ad1aaf
komakome <koumei.shindo@gmail.com>
2026-04-04 21:36:53 +0900

    Update questions.js

--- api/questions.js
+++ api/questions.js
@@ -35,7 +35,7 @@ module.exports = async (req, res) => {
 
       await sheets.spreadsheets.values.append({
         spreadsheetId: SPREADSHEET_ID,
-        range: "'Q&A管理'!A:C",
+       range: 'QA管理!A:C'
         valueInputOption: 'RAW',
         insertDataOption: 'INSERT_ROWS',
         requestBody: {
@@ -54,7 +54,7 @@ module.exports = async (req, res) => {
   try {
     const response = await sheets.spreadsheets.values.get({
       spreadsheetId: SPREADSHEET_ID,
-     range: "'Q&A管理'!A2:C", // ヘッダー行を除いた2行目以降
+     range: 'QA管理!A2:C' // ヘッダー行を除いた2行目以降
     });
 
     const rows = response.data.values || [];
